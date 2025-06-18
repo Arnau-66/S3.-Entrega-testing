13bf3c9806ce2a02174f132c90ef3d9635362c3e
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-18 11:31:12 +0200

    WIP Exercice 1

--- src/films.js
+++ src/films.js
@@ -1,9 +1,12 @@
 // Exercise 1: Get the array of all directors.
-function getAllDirectors(array) {
-  let result =  [];
-  console.log("EXERCICE 1 ->", result);
-  return result;
+
+const directorsArray = getAllDirectors(movies);
+
+function getAllDirectors(movies) {
+  return movies.map(movie => movie.director);
 }
+
+console.log(directorsArray);
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
